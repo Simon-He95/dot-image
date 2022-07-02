2393a157f28f6d854e4ec8875e11fa1965bb6025
Simon he <13917107460@163.com>
2022-07-02 23:52:59 +0800

    chore: update

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -3,8 +3,7 @@ import { animationFrameWrapper, randomRgb, toBase64 } from 'simon-js-tool'
 import { DotImageCanvas } from '../../src/DotImageCanvas.ts'
 import img from '../public/3.png'
 import img2 from '../public/2.jpg'
-const fontWeight = ref(1)
-const color = ref(randomRgb())
+const fontWeight = ref(3)
 
 const fileEl = ref(null)
 const uploadImage = ref('')
@@ -16,22 +15,12 @@ async function fileChange() {
   const base64 = await toBase64(file, 'file')
   uploadImage.value = base64
 }
-
-function onload1({ status }) {
-  const stop = animationFrameWrapper(() => {
-    if (fontWeight.value > 5)
-      fontWeight.value = 1
-    else fontWeight.value++
-    color.value = randomRgb()
-    stop()
-  }, 3000)
-}
 </script>
 
 <template>
   <main font-sans p=" y-10" text="center gray-700 dark:gray-200">
     <dot-text text="Dot Image" color="grey" font-size="30" font-weight="10" ma m-y-10 />
-    <DotImage :src="img" :color="color" :font-weight="fontWeight" w-20 />
+    <DotImage :src="img" bg-color="transparent" :font-weight="fontWeight" w-20 />
     <DotImage :src="img2" :onload="onload1" :font-weight="fontWeight" ma h-80 p-l-10 />
     <input ref="fileEl" type="file" hidden accept="image/*" @change="fileChange">
     <div>
